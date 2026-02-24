2589daf2a7407002582c3d2d24299cfd7ce8799e
Munshi Investment Club Developer <user@example.com>
2026-02-24 15:35:14 +0300

    chore(repo): remove client and electron wrapper from server repository

--- pos-electron/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronPOS', {
-    isElectron: true,
-    platform: process.platform,
-    getConfig: () => ipcRenderer.invoke('get-config'),
-    getPrinters: () => ipcRenderer.invoke('get-printers'),
-    printHtml: (payload) => ipcRenderer.invoke('print-html', payload)
-});
